6dbfcd036ca880372b7826ba3b048eac1bcf214b
RMavluda <85381387+RMavluda@users.noreply.github.com>
2023-11-30 14:34:56 +0500

    Add files via upload

---
+++ 0002_values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("go" + "lang")
+
+	fmt.Println("2+2 = ", 2 + 2)
+	fmt.Println("5.0/2/0 = ", 5.0/2.0)
+	fmt.Println(true && true)
+	fmt.Println(false || false)
+	fmt.Println(!false)
+}
